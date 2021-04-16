726cacb9d469bf4032c9d181181a38a6a3435407
Kayory <corvex166@gmail.com>
2021-04-17 00:50:02 +0200

    update: add comments

--- AE-Kontrollstrukturen/AE-Kontrollstrukturen/AE-Kontrollstrukturen.cpp
+++ AE-Kontrollstrukturen/AE-Kontrollstrukturen/AE-Kontrollstrukturen.cpp
@@ -6,12 +6,21 @@
 
 #include "Windows.h"
 
+
+/// <summary>
+/// Deklarierung für konstante Variablen, zum Konvertieren von verschiedenen Maßeinheiten.
+/// </summary>
 const double TEBIBYTE_MULTIPLIER = 1024. * 1024. * 1024. * 1024.;
 const double GIBIBYTE_MULTIPLIER = 1024. * 1024. * 1024.;
 const double MEBIBYTE_MULTIPLIER = 1024. * 1024.;
 const double KIBIBYTE_MULTIPLIER = 1024.;
 const double BYTE_MULTIPLIER = 8;
 
+/// <summary>
+/// Gibt die angegebene Nachricht in der Konsole aus, greift den Input vom user ab und gibt diesen an den callee zurück.
+/// </summary>
+/// <param name="message">Die Nachricht die ausgegeben wird.</param>
+/// <returns>Der Input den der User eingibt.</returns>
 std::string get_user_input(std::string message) noexcept
 {
     std::cout << message;
@@ -20,9 +29,14 @@ std::string get_user_input(std::string message) noexcept
     return _szInput;
 }
 
-std::string output_from_dynamic_type(int iDynamic) noexcept
+/// <summary>
+/// Gibt den Namen der Maßeinheit zurück, anhand eines angegebenen Integers, welcher für diese Maßeinheit im Menü festegelegt ist.
+/// </summary>
+/// <param name="InputType">Die Zahl die zur Identifizierung der Maßeinheit verwendet wird.</param>
+/// <returns>Den Namen der Maßeinheit.</returns>
+std::string get_type_name(int InputType) noexcept
 {
-    switch (iDynamic)
+    switch (InputType)
     {
         case 1:
             return "Bit";
@@ -40,67 +54,103 @@ std::string output_from_dynamic_type(int iDynamic) noexcept
     return "NONE";
 }
 
-double to_bit_from_dynamic(double dValue, int iDynamic)
+/// <summary>
+/// Konvertiert den angegebenen Input anhand eines Integers, welcher die Maßeinheit identifiziert, in Bits und gibt den wert an den callee zurück.
+/// </summary>
+/// <param name="InputValue">Der Wert der in Bits konvertiert werden soll.</param>
+/// <param name="InputType">Die Zahl die zur Identifizierung der Maßeinheit verwendet wird.</param>
+/// <returns>Den zu konvertierenden Wert in Bits.</returns>
+double convert_type_to_bits(double InputValue, int InputType)
 {
-    switch (iDynamic)
+    switch (InputType)
     {
         case 1:
-            return dValue;
+            return InputValue;
         case 2:
-            return dValue * BYTE_MULTIPLIER * 8;
+            return InputValue * BYTE_MULTIPLIER * 8;
         case 3:
-            return dValue * KIBIBYTE_MULTIPLIER * 8;
+            return InputValue * KIBIBYTE_MULTIPLIER * 8;
         case 4:
-            return dValue * MEBIBYTE_MULTIPLIER * 8;
+            return InputValue * MEBIBYTE_MULTIPLIER * 8;
         case 5:
-            return dValue * GIBIBYTE_MULTIPLIER * 8;
+            return InputValue * GIBIBYTE_MULTIPLIER * 8;
         case 6:
-            return dValue * TEBIBYTE_MULTIPLIER * 8;
+            return InputValue * TEBIBYTE_MULTIPLIER * 8;
     }
     return 0;
 }
 
-double to_dynamic_output(double dValue, int iDynamic)
+/// <summary>
+/// Konvertiert den angegebenen Input in Bits anhand eines Integers, welcher die Maßeinheit identifiziert, in die gewünsche Maßeinheit und gibt den wert an den callee zurück.
+/// </summary>
+/// <param name="InputValue">Der Wert in Bits der konvertiert werden soll.</param>
+/// <param name="InputType">Die Zahl die zur Identifizierung der Maßeinheit verwendet wird.</param>
+/// <returns>Den zu konvertierenden Wert in der gewünschten Maßeinheit.</returns>
+double convert_bits_to_type(double InputValue, int InputType)
 {
-    switch (iDynamic)
+    switch (InputType)
     {
         case 1:
-            return dValue;
+            return InputValue;
         case 2:
-            return dValue / BYTE_MULTIPLIER;
+            return InputValue / BYTE_MULTIPLIER;
         case 3:
-            return dValue / BYTE_MULTIPLIER / KIBIBYTE_MULTIPLIER;
+            return InputValue / BYTE_MULTIPLIER / KIBIBYTE_MULTIPLIER;
         case 4:
-            return dValue / BYTE_MULTIPLIER / MEBIBYTE_MULTIPLIER;
+            return InputValue / BYTE_MULTIPLIER / MEBIBYTE_MULTIPLIER;
         case 5:
-            return dValue / BYTE_MULTIPLIER / GIBIBYTE_MULTIPLIER;
+            return InputValue / BYTE_MULTIPLIER / GIBIBYTE_MULTIPLIER;
         case 6:
-            return dValue / BYTE_MULTIPLIER / TEBIBYTE_MULTIPLIER;
+            return InputValue / BYTE_MULTIPLIER / TEBIBYTE_MULTIPLIER;
     }
     return 0;
 }
 
-bool convert_all(int iDynamic)
+/// <summary>
+/// Greift den Input des Users ab und beginnt mit der Konvertierung in die verschiedenen Maßeinheiten.
+/// </summary>
+/// <param name="InputType">Ein Integer mit dem die Maßeinheit des Input identifiziert wird.</param>
+/// <returns>Einen Boolean welcher identifiziert ob die funktion erfolgreich ausgeführt wird.</returns>
+bool convert_all(int InputType)
 {
     std::cout << std::endl;
-    double _dInputValue = 0;
 
-    while (_dInputValue == 0)
+    /// <summary>
+    /// Deklarierung der Variable m_inputValue mit dem Wert 0.
+    /// </summary>
+    double m_inputValue = 0;
+
+    /// <summary>
+    /// Führt eine loop aus, welche nur unterbrochen wird, wenn der angegebene Input nicht gleich 0 ist.
+    /// </summary>
+    while (m_inputValue == 0)
     {
         try
         {
-            _dInputValue = std::atof(get_user_input("Geben Sie einen Wert ein: ").c_str());
+            /// <summary>
+            /// Konvertiert den Input von std:string zum Datentyp Double und speichert ihn in der Variable m_inputValue.
+            /// </summary>
+            m_inputValue = std::atof(get_user_input("Geben Sie einen Wert ein: ").c_str());
 
-            if (_dInputValue == 0)
+            /// <summary>
+            /// Überprüft ob der Input nicht gleich null ist, wenn ja wird eine Fehlermeldung ausgegeben und eine weitere Input abfrage durchgeführt.
+            /// </summary>
+            if (m_inputValue == 0)
             {
                 std::cout << "Die Eingabe muss groesser oder kleiner als 0 sein!" << std::endl;
                 continue;
             }
 
-            auto _dInputBits = to_bit_from_dynamic(_dInputValue, iDynamic);
+            /// <summary>
+            /// Konvertiert den angegebenen Wert in Bits und speichert diesen in der Variable m_inputInBits.
+            /// </summary>
+            auto m_inputInBits = convert_type_to_bits(m_inputValue, InputType);
 
+            /// <summary>
+            /// Führt eine loop mit allen verfügbaren Maßeinheit Identifikatoren aus 
+            /// </summary>
             for (int i = 1; i <= 6; i++)
-                std::cout << output_from_dynamic_type(i) << ": " << to_dynamic_output(_dInputBits, i) << std::endl;
+                std::cout << get_type_name(i) << ": " << convert_bits_to_type(m_inputInBits, i) << std::endl;
         }
         catch (const std::exception& errorCode)
         {
@@ -113,8 +163,10 @@ bool convert_all(int iDynamic)
     return true;
 }
 
-
-std::string display_menu_and_get_input() noexcept
+/// <summary>
+/// Zeigt die verscheidenen Auswahlmöglichkeiten in der Konsole an.
+/// </summary>
+void display_menu() noexcept
 {
     std::cout << std::fixed;
     std::cout << "1.) Eingabe in Bit." << std::endl;
@@ -125,27 +177,35 @@ std::string display_menu_and_get_input() noexcept
     std::cout << "6.) Eingabe in Tebibyte." << std::endl;
     std::cout << "0.) Beenden." << std::endl;
     std::cout << std::endl;
-    return get_user_input("Ihre Eingabe: ");
 }
 
+/// <summary>
+/// Die Hauptloop der Applikation. Hier wird das Menü angezeigt und die Maßeinheit der eingabe Festgelegt.
+/// </summary>
 void main_loop()
 {
+    /// <summary>
+    /// Eine Endlosschleife die läuft bis der User den Input für das beenden gibt.
+    /// </summary>
     while (true)
     {
+
         try
         {
-            int _iDynamic = std::stoi(display_menu_and_get_input());
+            display_menu();
 
-            if (_iDynamic == 0)
+            int m_inputType = std::stoi(get_user_input("Ihre Eingabe: "));
+
+            if (m_inputType == 0)
                 break;
-            if (_iDynamic < 0 || _iDynamic > 6)
+            if (m_inputType < 0 || m_inputType > 6)
             {
-                std::cout << "Die Eingabe muss zwischen 0 und 6 liegen. Ihre Eingabe: " << _iDynamic << std::endl << std::endl;
+                std::cout << "Die Eingabe muss zwischen 0 und 6 liegen. Ihre Eingabe: " << m_inputType << std::endl << std::endl;
                 std::this_thread::sleep_for(std::chrono::milliseconds(2500));
                 continue;
             }
 
-            convert_all(_iDynamic);
+            convert_all(m_inputType);
         }
         catch (const std::exception& errorCode)
         {
@@ -155,10 +215,25 @@ void main_loop()
     }
 }
 
+/// <summary>
+/// Der EntryPoint der Applikation.
+/// </summary>
+/// <returns>Einen Wert, welcher angibt, ob das Programm erfolgreich ausgefürt wurde. (0 = SUCCESS)</returns>
 int main() noexcept
 {
+    /// <summary>
+    /// Setzt den Titel der Konsole.
+    /// </summary>
     SetConsoleTitleA("Kontrollstrukturen und Verzweigungen");
+    
+    /// <summary>
+    /// Führt die main_loop aus.
+    /// </summary>
     main_loop();
+    
+    /// <summary>
+    /// Gibt den Wert 0 zurück und beendet die Applikation.
+    /// </summary>
     return 0;
 }
 
